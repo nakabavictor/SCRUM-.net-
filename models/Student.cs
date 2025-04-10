0b560f8f75630e01cca4ea19e3e63ec70cff271f
Victor de Castro Nakabayashi <127894385+nakabavictor@users.noreply.github.com>
2025-04-10 05:17:00 -0300

    Criacao classe student

---
+++ models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDBF.models;
+
+public class Student
+{
+    [Key]
+    [DisplayName]
+    public int Id { get; set; } 
+    
+    [Required(ErrorMessage = "informe o nome")]
+    [StringLength(50, ErrorMessage = "O nome deve conter no maximo 50 caracteres")]
+    [MinLength(5, ErrorMessage = "O nome deve conter no minimo 5 caracteres")]
+    [DisplayName("Nome Completo")]
+    public string Nome { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "informe o email")]
+    [EmailAddress(ErrorMessage = "E-mail invalido")]
+    [DisplayName("E-mail")]
+    public string Email { get; set; } = string.Empty;
+        
+}
